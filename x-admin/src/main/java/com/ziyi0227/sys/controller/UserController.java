dd62c73c596feba9ed91b3e7b04466e83c422182
ziyi0227 <1206881089@qq.com>
2023-10-28 21:11:00 +0800

    对接前端，用户列表查询接口

--- x-admin/src/main/java/com/ziyi0227/sys/controller/UserController.java
+++ x-admin/src/main/java/com/ziyi0227/sys/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.ziyi0227.sys.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ziyi0227.common.vo.Result;
 import com.ziyi0227.sys.entity.User;
 import com.ziyi0227.sys.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -52,6 +56,26 @@ public class UserController {
     public Result<?> logout(@RequestHeader("X-Token") String token){
         userService.logout(token);
         return Result.success();
+    }
+
+    @GetMapping("/list")
+    public Result<Map<String,Object>> getUserList(@RequestParam(value = "username",required = false) String username,
+                                                @RequestParam(value = "phone",required = false) String phone,
+                                                @RequestParam("pageNo") Long pageNo,
+                                                @RequestParam("pageSize") Long pageSize){
+
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(StringUtils.hasLength(username),User::getUsername,username);
+        wrapper.eq(StringUtils.hasLength(phone),User::getPhone,phone);
+
+        Page<User> page = new Page<>(pageNo,pageSize);
+        userService.page(page,wrapper);
+
+        Map<String,Object> data = new HashMap<>();
+        data.put("total",page.getTotal());
+        data.put("rows",page.getRecords());
+
+        return Result.success(data,"查询成功");
     }
 
 }
